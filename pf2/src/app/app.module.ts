c89b33d6d55cfbe2ff4a0e991eee890d11e6d602
99Ranged <jamartins@primeit.pt>
2017-07-28 17:50:19 +0100

    circle base

--- pf2/src/app/app.module.ts
+++ pf2/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PfolioComponent } from './pfolio.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PfolioComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PfolioComponent]
 })
 export class AppModule { }
---
+++ pf2/src/app/pfolio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pfolio-body',
+  templateUrl: 'pfolio.component.html',
+  styleUrls: ['./pfolio.component.css']
+})
+
+export class PfolioComponent {
+  constructor() { }
+
+
+}
