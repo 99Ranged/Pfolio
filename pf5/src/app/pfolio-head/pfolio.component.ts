ee571272789702f66812f6d096f0c21c48ad0d54
99Ranged <jamartins@primeit.pt>
2017-09-01 13:55:58 +0100

    Modified the random func to support more titles

--- pf5/src/app/pfolio-head/pfolio.component.ts
+++ pf5/src/app/pfolio-head/pfolio.component.ts
@@ -13,26 +13,32 @@ export class PfolioComponent  {
   stopchanging:boolean = false;
   counter:number = 0;
   randomletter:string = "";
+
+
   namegoeshere:string = 'COMPETÊNCIAS ACADÉMICAS';
   newnamegoeshere:string = '';
   possible = "ABCDEÊFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  setCharAt(index,chr) {
-    this.namegoeshere = this.namegoeshere.substring(0,index) + chr + this.namegoeshere.substring(index+1);
+  titlesarray=[this.namegoeshere,"belh"];
+
+
+  setCharAt(tindex, index,chr) {
+    this.titlesarray[tindex] = this.titlesarray[tindex].substring(0,index) + chr + this.titlesarray[tindex].substring(index+1);
   }
 
-  returnchangename(indextochange:number, finalletter:string )
+  returnchangename(indexoftitle:number, charindextochange:number, finalletter:string )
   {
     setTimeout(() => {
       //Loop to go through all string
      // for(var z = 0; z<this.namegoeshere.length;z++)
         //{
           //Check if caracter is a space, if so, don't change it
-          if(this.namegoeshere.charAt(indextochange) != ' ')
+          if(this.titlesarray[indexoftitle].charAt(charindextochange) != ' ')
             {
               this.randomletter = this.possible.charAt(Math.floor(Math.random() * this.possible.length));
               //This will replace a character in the random string
-              this.setCharAt(indextochange,this.randomletter);
+             // this.stringtochange = stringtochange.substring(0,indextochange) + this.randomletter + stringtochange.substring(indextochange+1);
+              this.setCharAt(indexoftitle,charindextochange,this.randomletter);
             }
 
         //}
@@ -46,28 +52,31 @@ export class PfolioComponent  {
       // How many times are we changing letters
       if(this.counter != 15)
       {
-        this.returnchangename(indextochange,finalletter);
+        this.returnchangename(indexoftitle,charindextochange,finalletter);
       }
       else
       {
         this.counter = 0;
-        this.setCharAt(indextochange,finalletter);
+        this.setCharAt(indexoftitle, charindextochange,finalletter);
       }
       // The timing between letter change
-    }, 80);
+    }, 40);
   }
 
 
 
-  changename()
+  changename(indexoftitle)
   {
-    for(var k = 0; k<this.namegoeshere.length;k++)
+    alert(indexoftitle);
+    //this.titlesarray[indexoftitle] = "cocoxixi";
+    //stringtochange = 'cocoxixi';
+    for(var k = 0; k<this.titlesarray[indexoftitle].length;k++)
      {
        // setTimeout(() =>
        // {
           //execute rotate for a given index
           //Method takes in letter index to be changed and the letter that is supposed to be in the end
-        this.returnchangename(k,this.namegoeshere.charAt(k));
+        this.returnchangename(indexoftitle, k,this.titlesarray[indexoftitle].charAt(k));
 
         //Time delay between character rotate finish
        // }, 100);
